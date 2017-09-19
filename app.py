0effade6d017df04a55e871efba2bc3a3dde27d7
Clare Sloggett <claresloggett@gmail.com>
2017-09-19 21:06:46 +1000

    Basic scatter plot.

--- app.py
+++ app.py
@@ -1,25 +1,47 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+
+import plotly.graph_objs as go
+
+import pandas as pd
+
+df = pd.read_csv('data/testdata.csv')
+fields = list(df.columns)
 
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Data embedding'),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    #html.Div(children='''
+    #    Dash: A web application framework for Python.
+    #'''),
+
+    html.Label('X-axis'),
+    dcc.Dropdown(
+        id='x_dropdown',
+        options = [{'label':v, 'value':v} for v in fields],
+        #value='MTL'
+    ),
+
+    html.Label('Y-axis'),
+    dcc.Dropdown(
+        id='y_dropdown',
+        options = [{'label':v, 'value':v} for v in fields],
+        #value='MTL'
+    ),
 
     dcc.Graph(
-        id='example-graph',
+        id='plain-scatterplot',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                go.Scatter(x=df['A'], y=df['B'], mode='markers',
+                           marker=dict(size=10))
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'title': 'Plot of B against A'
             }
         }
     )
