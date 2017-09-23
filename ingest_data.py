594d6759d47673e75b3ccf11cb95d0e36e28ea89
Clare Sloggett <claresloggett@gmail.com>
2017-09-23 15:43:19 +1000

    Refactor dataframe parsing.

--- ingest_data.py
+++ ingest_data.py
@@ -22,47 +22,52 @@ def parse_input(infile, separator):
     """
     df = pd.read_csv(infile, sep=separator, header=0)
 
+    # Check for RowType column
     if 'RowType' in df.columns:
-        data_rows = df['RowType']=='Data'
-        fieldinfo_rows = df['RowType']=='FieldInfo'
-        # data columns must at least exclude RowType column
-        # it can be further restricted later
-        data_columns = df.columns != 'RowType'
+        data_rows = df.index[df['RowType']=='Data']
+        fieldinfo_rows = df.index[df['RowType']=='FieldInfo']
         columntype_row = df.index[df['RowType']=='ColumnType']
-        if len(columntype_row) > 1:
-            raise ValueError("More than one ColumnType row in input data.")
-        if len(columntype_row) > 0:
-            columntypes = df.loc[columntype_row,:].iloc[0]
-            # Only handle numeric for now
-            # Can override previous data_columns; RowType should not be Numeric etc
-            data_columns = columntypes.isin(['Numeric'])
-            data = df.loc[data_rows, data_columns]
-            fields = list(df.columns[data_columns])
-            field_info = pd.DataFrame(index=fields)
-            field_info['FieldType'] = columntypes[data_columns]
-            sample_info = pd.DataFrame(index=df.index[data_rows])
-            sampleinfo_columns = (columntypes == 'RowInfo')
-            sample_info = df.loc[data_rows, sampleinfo_columns]
-        else:
-            print("No ColumnType column in input data; assuming all columns are numeric fields.")
-            data = df.loc[data_rows,data_columns]
-            sample_info = None
-            fields = list(df.columns[data_columns])
-            field_info = pd.DataFrame(index=fields)
-            field_info['FieldType'] = ['Numeric']*len(field_info)
     else:
-        print("No RowType column in input data; assuming all rows are data and all columns are numeric fields.")
-        data = df.loc[:,df.columns]
-        sample_info = None
-        fields = list(df.columns)
-        field_info = pd.DataFrame(index=fields)
-        field_info['FieldType'] = ['Numeric']*len(field_info)
+        print("No RowType column in input data; assuming all rows are data.")
+        data_rows = df.index
+        fieldinfo_rows = []
+        columntype_row = []
+
+    # Check for ColumnType row
+    if len(columntype_row) > 1:
+        raise ValueError("More than one ColumnType row in input data.")
+    elif len(columntype_row) == 0:
+        print("No ColumnType column in input data; assuming all columns data fields, unless named RowType.")
+        data_columns = df.columns[df.columns != 'RowType']
+        # TODO: try to auto-detect column type. For now, assume numeric.
+        columntypes = pd.Series(index=df.columns)
+        columntypes[data_columns] = 'Numeric'
+    else:
+        assert len(columntype_row) == 1
+        columntypes = df.loc[columntype_row,:].iloc[0]
+
+    # Extract data and metadata
+
+    # TODO: handle other column types
+    data_columns = df.columns[columntypes.isin(['Numeric'])]
+
+    data = df.loc[data_rows, data_columns]
+
+    field_info = pd.DataFrame(index=data_columns)
+    field_info['FieldType'] = columntypes[data_columns]
+
+    sample_info = pd.DataFrame(index=data_rows)
+    sampleinfo_columns = (columntypes == 'RowInfo')
+    sample_info = df.loc[data_rows, sampleinfo_columns]
+
     if data.shape[0]==0:
         raise ValueError("No data rows found.")
     if data.shape[1]==0:
         raise ValueError("No data fields found.")
+
     print("Data shape {}".format(data.shape))
     print("Data fields: {}".format(','.join(data.columns)))
     if sample_info is not None:
         print("Sample info fields: {}".format(','.join(sample_info.columns)))
+
     return (data, sample_info, field_info)
